c513463a4f0ac77a7de58033620cc37a2962d344
shang-xi <122677584+shang-xi@users.noreply.github.com>
2023-02-23 21:23:24 -0800

    x

--- env.sh
+++ env.sh
@@ -2,5 +2,7 @@ export AWS_ACCESS_KEY_ID=ASIAVT6UJRRCLMVRO642
 export AWS_SECRET_ACCESS_KEY=IKM3Lb5V5EPLPP2YJVhnxWHuptGSnYxOXN/zLrzV
 export AWS_SESSION_TOKEN=FwoGZXIvYXdzEHYaDOeVWnnkostMc8K8NCKtAWb+xZ2YYXE0/igoYCGRY4ciAm7rb3Aatc5DMpBYH+XVxz0/OdxiukXCoHfH5Q+aoHsHxarCYakht/ehGRSeGkp403kmOZjq8gJ8vUGtYTljM7glYldn2LpblcFmarWlno63Jhrz/l9tvsm1K0z9X/vNgEqWCq47F10FrBFzAs/Rf4bI3n6+zuAYzj2KrSCrdVNPcm2OjzvrT39tT/I4A9lM8MrnD9WTDMhpEC6HKLf94J8GMi3hg+ScdMrmqT++6DVTfAdJjFQHZ/IpAZtVWtI/HynLI/XVbaim5KkEwe6AdOE=
 
+pem="/Users/ryandsouza/Documents/dsc102/hw/pa1/dsc-pa1/ryan-dask-key.pem"
+alias sj="ssh -i $pem -L 8888:localhost:8888 ubuntu@34.209.238.171"
 
-alias sm="ssh -i ryan-dask-key.pem ubuntu@54.187.151.127"
+alias jn="jupyter notebook --port=8888"
